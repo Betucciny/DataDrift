3fc2d776d58e2fa468de5b1a701c7edd99e2499d
Betucciny <betohr48@gmail.com>
2025-03-18 18:03:35 -0600

    Minio to not ssl

--- server/minioClient.ts
+++ server/minioClient.ts
@@ -3,7 +3,8 @@ import { Client } from "minio";
 // Create a MinIO client instance
 const minioClient = new Client({
   endPoint: process.env.MINIO_URL ?? "",
-  useSSL: true,
+  // useSSL: true,
+  port: parseInt(process.env.MINIO_PORT ?? "3000"),
   accessKey: process.env.MINIO_ACCESSKEYID ?? "",
   secretKey: process.env.MINIO_SECRETACCESSKEY ?? "",
 });
